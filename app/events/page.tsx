572f08dd298d20471209431725f73cb9baada14f
Sophie Tonthat <145614128+soofles@users.noreply.github.com>
2026-07-13 21:44:19 -0400

    More basic styling, added basic layouts on events pages

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Events() {
     return (
         <div>
-            hi this is an event
+            <p>events page test</p>
+            <Link href="/events/1">click here for individual event page test</Link>
         </div>
     )
 }
--- components/event/EventInfo.tsx
+++ components/event/EventInfo.tsx
@@ -9,16 +9,37 @@
 
 import styles from "./EventInfo.module.css"
 import Image from "next/image"
+import favicon from "../../app/favicon.ico"
 
 function EventInfo() {
     return (
         <div className={styles.eventInfo}>
-            <Image
-                src=""
-                alt=""
-            />
+            <div className={styles.eventLogo}>
+                <Image
+                    width={100}
+                    height={100}
+                    src={favicon}
+                    alt="event logo"
+                />
+            </div>
+            <span className={styles.eventName}>Event Name</span>
             <div className={styles.eventInfoData}>
-                <h2></h2>
+                <div>
+                    <span>Date</span>
+                    <span>TBD</span>
+                </div>
+                <div>
+                    <span>Location</span>
+                    <span>TBD</span>
+                </div>
+                <div>
+                    <span>Prize</span>
+                    <span>TBD</span>
+                </div>
+                <div>
+                    <span>Teams</span>
+                    <span>TBD</span>
+                </div>
             </div>
         </div>
     )
